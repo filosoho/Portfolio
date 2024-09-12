e34a46d99509ea4223c05f25e46d3a3ee32591ad
Anna <filosoho@gmail.com>
2024-09-12 09:30:38 +0100

    add sections folder with Navbar.jsx and basic setup for Navbar, clean and setup App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import React from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Sparkles } from "@react-three/drei";
+import Navbar from "./sections/Navbar";
 
-const RotatingCube = () => {
-  const meshRef = React.useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1, 1, 1]} />
-      <meshLambertMaterial color="#468585" emissive="#468585" />
-
-      <Sparkles count={100} scale={1} speed={0.002} noise={0.2} color="black" />
-    </mesh>
-  );
-};
 const App = () => {
   return (
-    <Canvas
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-
-      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-
-      <color attach="background" args={["#F0F0F0"]} />
-      <RotatingCube />
-    </Canvas>
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+    </main>
   );
 };
 
