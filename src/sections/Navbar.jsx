706692fc759bafe28b286a87afd370bcd35086ad
Anna <filosoho@gmail.com>
2024-11-20 15:15:49 +0000

    remove blank link from logo in navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -25,13 +25,7 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 right-0  bg-transparent backdrop-filter backdrop-blur-md">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
-          <a
-            href="/"
-            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors "
-          >
-            <img className="logo-img" src={logo} alt="filosoho space logo" />
-          </a>
-
+          <img className="logo-img" src={logo} alt="filosoho space logo" />
           <button
             onClick={toggleMenu}
             className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
